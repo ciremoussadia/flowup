4891e73a1bbca6350f4a2a358f61db31de8d5d81
Cire DIA <ciremoussadia@gmail.com>
2020-05-06 04:47:48 +0000

    refactor: Refactor Subscription request
    DRY the subscription request.

--- spec/requests/create_subscription_spec.rb
+++ spec/requests/create_subscription_spec.rb
@@ -3,11 +3,13 @@
 require 'rails_helper'
 
 describe 'create subscription', type: :request do
+  let(:endpoint) { '/subscriptions' }
+  let(:subscription_params) { attributes_for(:subscription) }
+  subject { post endpoint, params: { subscription: subscription_params } }
+
   context 'with valid data' do
     it 'saves to db and redirect to home' do
-      expect  do
-        post '/subscriptions', params: { subscription: attributes_for(:subscription) }
-      end.to change(Subscription, :count)
+      expect { subject }.to change(Subscription, :count)
 
       expect(response).to redirect_to root_path
       expect(flash[:success]).to be_present
@@ -15,13 +17,13 @@ describe 'create subscription', type: :request do
   end
 
   context 'with invalid data' do
+    let(:subscription_params) { attributes_for(:subscription, name: '') }
     it 'do not save to db and reload form' do
-      expect do
-        post '/subscriptions', params: { subscription: attributes_for(:subscription, name: '') }
-      end.to_not change(Subscription, :count)
+      expect { subject }.to_not change(Subscription, :count)
     end
+
     it 'reload form' do
-      post '/subscriptions', params: { subscription: attributes_for(:subscription, name: '') }
+      subject
       expect(response.code).to eq('200')
     end
   end
